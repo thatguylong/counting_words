ef9a01c2e19bb13bbbb0a442c9ce8a7573239b66
thatguylong <thatguylong@gmail.com>
2016-07-26 23:43:32 -0700

    revise ports

--- counter.rb
+++ counter.rb
@@ -1,6 +1,32 @@
 require 'sinatra'
 require 'json'
 require 'slim'
+require 'thin'
+
+class MyThinBackend < ::Thin::Backends::TcpServer
+  def initialize(host, port, options)
+    super(host, port)
+    @ssl = true
+    @ssl_options = options
+  end
+end
+
+configure do
+  set :environment, :production
+  set :bind, '0.0.0.0'
+  set :port, 443
+  set :server, "thin"
+  class << settings
+    def server_settings
+      {
+        :backend          => MyThinBackend,
+        :private_key_file => File.dirname("/etc/apache2/ssl") + "/server.key",
+        :cert_chain_file  => File.dirname("/etc/apache2/ssl") + "/server.crt",
+        :verify_peer      => false
+      }
+    end
+  end
+end
 
 get '/' do 
   slim :index 
